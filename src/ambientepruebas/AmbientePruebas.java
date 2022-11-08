1e1ae021d76ca11be22ace2d56da011f63e2f9f3
KarinaMadrigal <101362675+KarinaMadrigal@users.noreply.github.com>
2022-11-07 22:06:05 -0600

    Inicio

---
+++ src/ambientepruebas/AmbientePruebas.java
@@ -0,0 +1,17 @@
+package ambientepruebas;
+
+/**
+ *
+ * @author Karina Madrigal
+ */
+public class AmbientePruebas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+    }
+    
+}
