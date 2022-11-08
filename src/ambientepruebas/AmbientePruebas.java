5c0c0460b4eb52bf6b58a89fa2a879f085d4f3e7
KarinaMadrigal <101362675+KarinaMadrigal@users.noreply.github.com>
2022-11-07 22:08:14 -0600

    Instalación Selenium

--- src/ambientepruebas/AmbientePruebas.java
+++ src/ambientepruebas/AmbientePruebas.java
@@ -1,17 +1,33 @@
 package ambientepruebas;
 
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 /**
  *
  * @author Karina Madrigal
  */
 public class AmbientePruebas {
 
+    static WebDriver driver;
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        
+
+        String url = "https://www.nacion.com/"; //probando la página de La Nacion CR
+
+        System.setProperty("webdriver.chrome.driver",
+                "C:\\Users\\Usuario\\Documents\\Sistemas"
+                + "\\2022\\Cuatri III\\Calidad Software"
+                + "\\Selenium\\chromedriver\\chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get(url);
+        System.out.println("Page title is " + driver.getTitle());
+
+//        driver.quit();
     }
-    
+
 }
---
+++ src/ambientepruebas/Referencia.java
@@ -0,0 +1,16 @@
+
+package ambientepruebas;
+
+/**
+ *
+ * @author Karina Madrigal
+ */
+public class Referencia {
+
+    /*
+    La instalación y código de prueba se realizó
+    con base en lo mostrado en 
+    https://www.youtube.com/watch?v=fFGN4P7PKJs
+    */
+    
+}
